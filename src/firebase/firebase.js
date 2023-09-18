61721e61714b47aac43daa62424763a2a7070c18
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-09-18 17:38:47 -0300

    chore: get form data

---
+++ src/firebase/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: 'AIzaSyBTbJEkGVtee4yYjLEhEY4-WnwtAbakQwU',
+	authDomain: 'boardgamers-do-es.firebaseapp.com',
+	projectId: 'boardgamers-do-es',
+	storageBucket: 'boardgamers-do-es.appspot.com',
+	messagingSenderId: '211647394209',
+	appId: '1:211647394209:web:c30f302682597e2da6dbe0',
+	measurementId: 'G-BCFXP1XGMP',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { app, db };
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,47 @@
 import styles from './Home.module.css';
+import { db } from '../../firebase/firebase';
+import { addDoc, collection, getDocs } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+
+interface Participant {
+	name: string;
+	phone: string;
+}
 
 export function Home() {
+	const [participants, setParticipants] = useState<Participant[]>([]);
+	const [newParticipantName, setNewParticipantName] = useState('');
+	const [newParticipantPhone, setNewParticipantPhone] = useState('');
+
+	async function getParticipants() {
+		try {
+			const querySnapshot = await getDocs(collection(db, 'participants'));
+			const recoveredData = querySnapshot.docs.map((doc) =>
+				doc.data()
+			) as Participant[];
+			setParticipants(recoveredData);
+		} catch (error) {
+			console.log('Error getting participants: ', error);
+		}
+	}
+
+	async function addParticipant() {
+		try {
+			await addDoc(collection(db, 'participants'), {
+				name: newParticipantName,
+				phone: newParticipantPhone,
+			});
+      setNewParticipantName('');
+      setNewParticipantPhone('');
+		} catch (error) {
+			console.log('Error adding participant: ', error);
+		}
+	}
+
+	useEffect(() => {
+		getParticipants();
+	}, [participants]);
+
 	return (
 		<main className={styles.main}>
 			<h2>Próxima jogatina:</h2>
@@ -17,22 +58,39 @@ export function Home() {
 			<form action='' className={styles.form}>
 				<div className={styles.data}>
 					<label htmlFor='name'>Nome:</label>
-					<input type='text' name='name' id='name' />
+					<input
+						type='text'
+						name='name'
+						id='name'
+						placeholder='Insira seu nome...'
+						value={newParticipantName}
+						onChange={(e) => setNewParticipantName(e.target.value)}
+					/>
 				</div>
 
 				<div className={styles.data}>
 					<label htmlFor='phone'>Telefone:</label>
-					<input type='text' name='phone' id='phone' />
+					<input
+						type='text'
+						name='phone'
+						id='phone'
+						placeholder='DDD9XXXXXXXX'
+						value={newParticipantPhone}
+						onChange={(e) => setNewParticipantPhone(e.target.value)}
+					/>
 				</div>
 
-				<button type='submit'>Confirmar participação</button>
+				<button type='reset' onClick={addParticipant}>
+					Confirmar participação
+				</button>
 			</form>
 
 			<div>
 				<h3 className={styles.text}>Participantes confirmados:</h3>
 				<ul className={styles.list}>
-					<li>Luara</li>
-					<li>Raira</li>
+					{participants.map((participant) => (
+						<li key={participant.phone}>{participant.name}</li>
+					))}
 				</ul>
 			</div>
 		</main>
