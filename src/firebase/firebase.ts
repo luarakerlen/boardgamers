6a7633ea0a62ff0e11d9b1e8d988f05f29ecc5ea
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-09-18 23:41:22 -0300

    chore: reinstall firestore

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: 'AIzaSyBTbJEkGVtee4yYjLEhEY4-WnwtAbakQwU',
-	authDomain: 'boardgamers-do-es.firebaseapp.com',
-	projectId: 'boardgamers-do-es',
-	storageBucket: 'boardgamers-do-es.appspot.com',
-	messagingSenderId: '211647394209',
-	appId: '1:211647394209:web:c30f302682597e2da6dbe0',
-	measurementId: 'G-BCFXP1XGMP',
+  apiKey: "AIzaSyDDyy6V74dpby7yrveGLcS0Ei7aRBZhskw",
+  authDomain: "boardgamers-es.firebaseapp.com",
+  projectId: "boardgamers-es",
+  storageBucket: "boardgamers-es.appspot.com",
+  messagingSenderId: "178438039571",
+  appId: "1:178438039571:web:92d089eb156341a4bab7e6",
+  measurementId: "G-W13BQ1WY1P"
 };
 
 // Initialize Firebase
